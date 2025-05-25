35997043412c49f58d0dd0ad070e6433fa3535d5
Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>
2025-05-25 14:23:12 +0530

    feat: add converters for visibility and boolean checks in MoodEntryWizard steps

---
+++ src/mood-moments/Converters/NullOrEmptyToBoolConverter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace mood_moments.Converters
+{
+    public class NullOrEmptyToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+                return !string.IsNullOrEmpty(str);
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/mood-moments/Converters/StepToFinishVisibilityConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace mood_moments.Converters
+{
+    public class StepToFinishVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int step)
+            {
+                // Only show Finish button on the last step (assumed to be 6)
+                return step == 6;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
